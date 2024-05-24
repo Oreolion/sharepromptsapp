7d5cb13cd6cc1c21c0cac141d3635e4cd4d41811
oreolion <remyoreo11@gmail.com>
2024-05-24 02:32:57 -0700

    add: update homepage and add profile page;

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((item) => {
+        return <PromptCard
+          key={item.id}
+          post={item}
+          handleTagClick={handleTagClick}
+        ></PromptCard>;
+      })}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
 
-export default Feed
+  useEffect(() => {
+    
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      console.log(response)
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} handleTagCLick={() => {}}></PromptCardList>
+    </section>
+  );
+};
+
+export default Feed;
